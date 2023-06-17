1ba533620e3fb1122e2cdb28aa228871c9976885
Bartosz Dutka <dutkabar@gmail.com>
2023-06-17 11:43:38 +0200

    added balance history

--- budgetwebapp/budget/admin.py
+++ budgetwebapp/budget/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import MoneyAccount, BudgetExpenseEntry, Category, MainCategory, SubCategory
+from .models import MoneyAccount, BudgetExpenseEntry, Category, MainCategory, SubCategory, BalanceHistory
 
 admin.site.register(MoneyAccount)
 admin.site.register(BudgetExpenseEntry)
 admin.site.register(Category)
 admin.site.register(MainCategory)
 admin.site.register(SubCategory)
+admin.site.register(BalanceHistory)
