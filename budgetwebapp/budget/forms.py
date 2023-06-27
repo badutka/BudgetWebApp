ec01bf71958e92b638833a35fac57dc72242f126
Bartosz Dutka <dutkabar@gmail.com>
2023-06-27 17:13:06 +0200

    added calendar widget to form

--- budgetwebapp/budget/forms.py
+++ budgetwebapp/budget/forms.py
@@ -1,11 +1,20 @@
 from django import forms
+from django.forms import SelectDateWidget
 from .models import Transaction, Category
+from django.forms import DateInput
+
+
+class DateInput(DateInput):
+    input_type = 'date'
 
 
 class BudgetExpenseEntryForm(forms.ModelForm):
+    date = forms.DateField(widget=DateInput)
+    description = forms.CharField(widget=forms.Textarea(), required=False)
+
     class Meta:
         model = Transaction
-        fields = ['date', 'category', 'amount', 'origin', 'destination']
+        fields = ['date', 'category', 'amount', 'origin', 'destination', 'description']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
