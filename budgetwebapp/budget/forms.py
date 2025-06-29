bcf1b8a39a716f1beedbc53bebb76c94cdeb2b55
Bartosz Dutka <dutkabar@gmail.com>
2025-06-29 21:31:27 +0200

    removed older version of transaction form

--- budgetwebapp/budget/forms.py
+++ budgetwebapp/budget/forms.py
@@ -8,37 +8,6 @@ from django.core.exceptions import ValidationError
 class CustomDateInput(DateInput):
     input_type = 'date'
 
-
-# class BudgetExpenseEntryForm(forms.ModelForm):
-#     date = forms.DateField(widget=CustomDateInput)
-#     description = forms.CharField(widget=forms.Textarea(), required=False)
-#
-#     class Meta:
-#         model = Transaction
-#         fields = ['date', 'category', 'amount', 'origin', 'destination', 'description']
-#
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.fields['category'].queryset = Category.objects.all()
-#
-#     def clean(self):
-#         cleaned_data = super().clean()
-#         category = cleaned_data.get('category')
-#
-#         if cleaned_data.get('origin') == 'OUT':
-#             transaction_type = 'INCOMING'
-#         elif cleaned_data.get('destination') == 'OUT':
-#             transaction_type = 'OUTGOING'
-#         else:
-#             transaction_type = 'INNER'
-#         print(transaction_type)
-#         print(category.transaction_type)
-#         if transaction_type and category:
-#             if transaction_type != category.transaction_type:
-#                 raise ValidationError(f"Transaction type does not match category type {transaction_type} - {category.transaction_type}")
-#
-#         return cleaned_data
-
 class BudgetExpenseEntryForm(forms.ModelForm):
     date = forms.DateField(widget=CustomDateInput)
     description = forms.CharField(widget=forms.Textarea(), required=False)
