05e0009b2e9bde6f91cd78c401be558728399710
Bartosz Dutka <dutkabar@gmail.com>
2025-06-29 20:59:17 +0200

    added getlist filter

--- budgetwebapp/budget/templatetags/template_filters.py
+++ budgetwebapp/budget/templatetags/template_filters.py
@@ -1,7 +1,13 @@
 from django.template.defaulttags import register
 
+
 # Custom template filter to get data from a dictionary using key in template
 
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.filter
+def getlist(qdict, key):
+    return qdict.getlist(key)
