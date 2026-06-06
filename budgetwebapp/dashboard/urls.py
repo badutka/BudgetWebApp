209473bf9fc288011ce3508c609c7fb717caddd2
Bartosz Dutka <dutkabar@gmail.com>
2026-06-06 19:00:58 +0200

    introduced more generic app layout with new base template

--- budgetwebapp/dashboard/urls.py
+++ budgetwebapp/dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.index, name='home'),
+    path('home/', views.index, name='home'),
     path('<slug:slug>/', views.dashboard_view, name='dashboard'),
     path('api/widgets/update-layout/', views.update_widget_layout, name='update_widget_layout'),
     path('api/widgets/<uuid:widget_id>/sidebar/', views.dashboard_sidebar_content, name='widget_sidebar'),
